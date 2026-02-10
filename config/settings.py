8aa7524d21debe8bd8270ca7099acd720c171f5e
thasneem04 <thasneem.explorer5@gmail.com>
2026-02-10 13:12:15 +0530

    settings

--- config/settings.py
+++ config/settings.py
@@ -153,20 +153,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("MYSQLDATABASE"),
+        "USER": os.getenv("MYSQLUSER"),
+        "PASSWORD": os.getenv("MYSQLPASSWORD"),
+        "HOST": os.getenv("MYSQLHOST"),
+        "PORT": int(os.getenv("MYSQLPORT")),
         "OPTIONS": {
-            "ssl": {
-                "ca": None
-            },
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4",
         },
     }
 }
+
 
 
 
