0b9414fdfd67f3e9da0c5314b940ea074297559b
thasneem04 <thasneem.explorer5@gmail.com>
2026-02-10 19:04:38 +0530

    remove password hashing for deployment issue

--- config/settings.py
+++ config/settings.py
@@ -108,12 +108,9 @@ SELLER_USERNAMES = [
 ]
 
 PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.BCryptPasswordHasher",
     "django.contrib.auth.hashers.PBKDF2PasswordHasher",
-    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
-    "django.contrib.auth.hashers.Argon2PasswordHasher",
-    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
+
 
 
 
